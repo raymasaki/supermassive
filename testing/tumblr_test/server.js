78a54fb6ad79fb0cb2fef792bcca560d07b98a7a
Alex KOVEOS <alexkoveos@Alexs-MacBook-Pro-2.local>
2015-11-23 11:37:15 -0500

    tumblr_test

--- testing/tumblr_test/server.js
+++ testing/tumblr_test/server.js
@@ -18,7 +18,7 @@ app.get("/", function(req, res) {
 });
 
 app.get('/search/:search_term', function(req, res) {
-  request('http://api.tumblr.com/v2/tagged?tag=' + encodeURI(req.params.search_term)+'&api_key=bhFzZZxxHsx7WOdVw9v2Ysc99PwlEFKPfEUnudbtJh0xvLGnN9', function(error, response, body) {
+  request(('http://api.tumblr.com/v2/tagged?tag='+encodeURI(req.params.id_search+&'api_key='+encodeURI(TUMBLER_CONSUMER_KEY)+ '&limit=1', function(error, response, body) {
     res.send(body);
   });
 });
@@ -31,14 +31,14 @@ app.get('/random', function(req, res) {
 });
 
 app.get('/kittens', function(req, res) {
-  request('http://api.tumblr.com/v2/tagged?tag=kittens&api_key=bhFzZZxxHsx7WOdVw9v2Ysc99PwlEFKPfEUnudbtJh0xvLGnN9&limit=1',
+  request('http://api.tumblr.com/v2/tagged?tag=kittens&api_key='+encodeURI(TUMBLER_CONSUMER_KEY)+'&limit=1',
   	function(error, response, body) {
   	res.send(body)
   });
 });
 
 app.get('/id/:id_search', function(req, res) {
-  request('http://api.tumblr.com/v2/tagged?tag='+encodeURI(req.params.id_search+'&api_key=bhFzZZxxHsx7WOdVw9v2Ysc99PwlEFKPfEUnudbtJh0xvLGnN9&limit=1'), function(error, response, body) {
+  request('http://api.tumblr.com/v2/tagged?tag='+encodeURI(req.params.id_search+&'api_key='+encodeURI(TUMBLER_CONSUMER_KEY)+ '&limit=1', function(error, response, body) {
     res.send(body);
   });
 });
