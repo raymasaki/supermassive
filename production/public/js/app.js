5b21958612a4ef9f24239700b2577ee27db38fbb
Ray Masaki <ray@raymasaki.com>
2015-11-24 11:36:42 -0500

    pushing progress on autoplay

--- production/public/js/app.js
+++ production/public/js/app.js
@@ -1,41 +1,96 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
   console.log('loaded');
 
-  // random text click =========================================================
+  // random autoplay click =========================================================
 
-  $('#random-text').click(function(e) {
+  $('#random-auto').click(function(e) {
     e.preventDefault();
 
     $.get('/texts/random', renderSingleText, 'json');
   });
 
+
+  // random text click =========================================================
+
+  $('#random-text').click(fetchTextsJson);
+
   // random video click =========================================================
 
-  $('#random-video').click(function(e) {
-    e.preventDefault();
-
-    $.get('/videos/random', renderSingleVideo, 'json');
-  });
+  $('#random-video').click(fetchVideosJson);
 
   // random gif click =========================================================
 
-  $('#random-gif').click(function(e) {
-    e.preventDefault();
-
-    $.get('/gifs/random', renderSingleGif, 'json');
-  });
+  $('#random-gif').click(fetchGifsJson);
 
   // random image click =========================================================
 
-  $('#random-image').click(function(e) {
-    e.preventDefault();
-
-    $.get('/images/random', renderSingleImage, 'json');
-  });
+  $('#random-image').click(fetchImagesJson);
 
 
 });
+
+
+// =============================================================================
+// FETCH DATA  =================================================================
+// =============================================================================
+
+
+// FETCH TEXT RANDOM  =========================================================
+
+var fetchTextsJson = function(data) {
+  $.get('/texts/random', renderSingleText, 'json');
+};
+
+// FETCH VIDEO RANDOM  =========================================================
+
+var fetchVideosJson = function(data) {
+  $.get('/videos/random', renderSingleVideo, 'json');
+};
+
+// FETCH GIF RANDOM  =========================================================
+
+var fetchGifsJson = function(data) {
+  $.get('/gifs/random', renderSingleGif, 'json');
+};
+
+// FETCH IMAGE RANDOM  =========================================================
+
+var fetchImagesJson = function(data) {
+  $.get('/images/random', renderSingleImage, 'json');
+};
+
+
+// =============================================================================
+// TIMER FUNCTIONS  ===========================================================
+// =============================================================================
+
+
+var textsTimer = function() {
+  window.setTimeout(fetchTextsJson, 5000);
+};
+var videosTimer = function() {
+  window.setTimeout(fetchVideosJson, 5000);
+};
+var gifsTimer = function() {
+  window.setTimeout(fetchGifsJson, 5000);
+};
+var imagesTimer = function() {
+  window.setTimeout(fetchImagesJson, 5000);
+};
+
+var clearTimers = function() {
+  window.clearTimeout(textsTimer);
+  window.clearTimeout(videosTimer);
+  window.clearTimeout(gifsTimer);
+  window.clearTimeout(imagesTimer);
+};
+
+
+// =============================================================================
+// RENDER FUNCTIONS  ===========================================================
+// =============================================================================
+
 
 // RENDER SINGLE TEXT  =========================================================
 
@@ -51,6 +106,12 @@ var renderSingleText = function(data) {
   $container.empty();
 
   $container.html('<p>' + post_summary + ' – @' + post_user + '</p>');
+
+  // clears all running timeouts
+  clearTimers();
+
+  // sets new fetch
+  textsTimer();
 
 };
 
@@ -81,6 +142,12 @@ var renderSingleGif = function(data) {
 
   $container.html('<img src=' + gif_url + ' width="100%">');
 
+  // clears all running timeouts
+  clearTimers();
+
+  // sets new fetch
+  gifsTimer();
+
 };
 
 
@@ -95,5 +162,11 @@ var renderSingleImage = function(data) {
   $container.empty();
 
   $container.html('<img src=' + image_url + ' width="100%">');
+
+  // clears all running timeouts
+  clearTimers();
+
+  // sets new fetch
+  imagesTimer();
 
 };
