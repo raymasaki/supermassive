1973e582ec98556aac51c4fd9dbff43d62e2668d
Alex KOVEOS <alexkoveos@Alexs-MBP-2.fios-router.home>
2015-11-27 08:09:59 -0500

    front end changes added css effects for border, loading ejs, profile ejs

--- testing/front_end_test/public/js/app.js
+++ testing/front_end_test/public/js/app.js
@@ -1,4 +1,85 @@
 $(document).ready(function () {
   console.log('loaded');
 
+
+  /* Making Logo the home link *******************************/
+  $(".logo-login").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  /* Making Divs links to different paths ********************/
+
+  $(".top-border").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  $(".bottom-border").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  $(".right-border").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  $(".left-border").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  /* Greeting will randomize when user logs in  ************/
+
+  var myGreeting = new Array();
+    myGreeting[0] = "Ahoy ";
+    myGreeting[1] = "Looking good today ";
+    myGreeting[2] = "Greetings ";
+    myGreeting[3] = "Howdy ";
+    myGreeting[4] = "You're my best friend ";
+    myGreeting[5] = "Hi there ";
+    myGreeting[6] = "Missed you ";
+    myGreeting[7] = "Hello ";
+    myGreeting[8] = "Hiya ";
+    myGreeting[9] = "What's cracking "
+    myGreeting[10] = "Find something fun today "
+  var myRandom = Math.floor(Math.random()*myGreeting.length);
+
+  $('#myGreeting').html(myGreeting[myRandom]);
+
+/* Arrow key events for navigating site ******************/
+
+  $(document).keydown(function(e){
+      if (e.keyCode === 38)
+        console.log( "up arrowkey pressed" );
+      else if (e.keyCode === 40)
+        console.log( "down arrowkey pressed" );
+      else if (e.keyCode === 37)
+        console.log( "left arrowkey pressed" );
+      else (e.keyCode === 39)
+        console.log( "right arrowkey pressed" );
+  });
+
+/*
+  $(document).keydown(function(e){
+      if (e.keyCode = 40)
+        console.log( "down arrowkey pressed" );
+  });
+
+  $(document).keydown(function(e){
+      if (e.keyCode = 37)
+        console.log( "left arrowkey pressed" );
+  });
+
+  $(document).keydown(function(e){
+      if (e.keyCode = 39)
+        console.log( "right arrowkey pressed" );
+  });
+
+  $(document).keydown(function(e){
+      if (e.keyCode = 36)
+        console.log( "spacebar pressed" );
+  });
+*/
 });
